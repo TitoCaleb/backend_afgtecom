5d06c4172c77c248f00b06d219ca0ca64a3d6d0f
TitoCaleb <calebcastro2604@gmail.com>
2024-08-03 00:42:59 -0500

    feat: add changes to module categories

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './controller/categories.controller';
 import { CategoriesService } from './service/categories.service';
+import { CategoriesRepositoryImpl } from './repository/categories.repository';
+import { Category } from 'src/domain/Category';
+import { Device } from 'src/domain/Device';
+import { Client } from 'src/domain/Client';
+import { Token } from 'src/domain/Token';
+import { SecurityRepositoryImpl } from '../security/repository/security.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category, Device, Client, Token])],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [
+    CategoriesService,
+    CategoriesRepositoryImpl,
+    SecurityRepositoryImpl,
+  ],
 })
 export class CategoriesModule {}
